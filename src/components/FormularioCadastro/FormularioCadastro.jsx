d0d2da81a54f5ccf7b75f7e16de8ca33314dd2cc
carlosbertarinijr <carlos.junior@codetec.io>
2020-10-13 16:12:12 -0300

    Renderizacao condicional

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
+import { Typography } from '@material-ui/core';
+
 
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+    const [etapaAtual, setEtapaAtual] = useState(1);
 
+    function formularioAtual(etapa) {
+        switch (etapa) {
+            case 0:
+                return <DadosUsuario />;
+            case 1:
+                return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />;
+            case 2:
+                return <DadosEntrega />;
+
+            default:
+                return <Typography>Erro</Typography>
+        }
+    }
     return (
         <>
-       <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-       <DadosUsuario />
-       <DadosEntrega />
-       </>
-    )
+            {formularioAtual(etapaAtual)}
+        </>
+    );
 }
-
 export default FormularioCadastro;
